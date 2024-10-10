ac80f8b06128b85412aadb0432bb328bc680cc35
LevMikhaylov <152269424+LevMikhaylov@users.noreply.github.com>
2024-10-10 17:11:03 +0300

    Create ArrayList task1.java

---
+++ ArrayList task1.java
@@ -0,0 +1,16 @@
+package ru.ithub.javatrainj;
+import java.util.ArrayList;
+import java.util.List;
+public class lol {
+    public static void main(String[] args) {
+        List<String> l = new ArrayList<>();
+        l.add("abc");
+        l.add("def");
+        l.add("ghi");
+        l.add("klm");
+        l.add("nop");
+        for(String s:l){
+            System.out.println(s);
+        }
+    }
+}
