b096115aad58571bf1f8728633480846c8ae259d
LevMikhaylov <152269424+LevMikhaylov@users.noreply.github.com>
2024-10-11 19:13:52 +0300

    Update CaesarEncrypter.java

--- CaesarEncrypter.java
+++ CaesarEncrypter.java
@@ -1,15 +1,7 @@
 package ru.ithub.javatrainj;
 public class CaesarEncrypter {
-    public static String encrypt(String text, int key) {
-
-        StringBuilder encrypted = new StringBuilder();
-        for (char character : text.toCharArray()) {
-            if (Character.isLetter(character)) {
-                char base = Character.isLowerCase(character) ? 'a' : 'A';
-                character = (char) ((character - base + key) % 26 + base);
-            }
-            encrypted.append(character);
-        }
-        return encrypted.toString();
+     public static String encrypt(String text, int key) {
+        TextShifter ts = new TextShifter();
+        return ts.shiftText(text, key);
     }
 }
