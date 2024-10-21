4465d422c96384a343fa1612a1fc7e5f8e0efe5b
LevMikhaylov <152269424+LevMikhaylov@users.noreply.github.com>
2024-10-21 13:27:18 +0300

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,1 +1,30 @@
+import java.time.LocalDate;
 
+public class Task {
+    private String name;
+    private LocalDate create_date;
+    public Task(String name, LocalDate create_date) throws Exception{
+        this.name = name;
+        if(create_date.isAfter(LocalDate.now())||create_date.equals(LocalDate.now())){
+            this.create_date = create_date;
+        }
+        else{
+            throw new Exception("Дата формирования задачи должна быть после текущей даты или равна ей");
+        }
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(LocalDate create_date) {
+        this.create_date = create_date;
+    }
+}
