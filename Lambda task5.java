66f325eca62fc94d5987c6358881af75ae632dd5
LevMikhaylov <152269424+LevMikhaylov@users.noreply.github.com>
2024-11-11 11:27:20 +0300

    Update Lambda task5.java

--- Lambda task5.java
+++ Lambda task5.java
@@ -1,1 +1,20 @@
+package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main{
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
+        words.add("apple");
+        words.add("banana");
+        words.add("avocado");
+        words.add("grape");
+        words.add("orange");
+
+        words.forEach(word -> {
+            long count = word.chars().filter(ch -> ch == 'a').count();
+            System.out.println("Слово: " + word + ", количество 'a': " + count);
+        });
+    }
+}
